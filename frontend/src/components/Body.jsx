1815942e22a1680a637f3f0cfd4e1d1ac7c73223
kirtiraj22 <kirtirajthakor064@gmail.com>
2023-11-06 23:58:52 +0530

    fetched restaurant's data from swiggy's api

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,5 +1,42 @@
+import { useState, useEffect } from 'react';
 const Body = () => {
-    return <div>Body</div>;
+    const [Restaurants, setRestaurants] = useState();
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            'https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.3071588&lng=73.1812187&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+        );
+        const jsonData = await data.json();
+        // console.log(jsonData);
+        const resData = jsonData?.data?.cards[2].card;
+        setRestaurants(resData);
+        setListOfRestaurants(resData?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+    // console.log(Restaurants.card.header.title);
+    console.log(listOfRestaurants);
+
+    return (
+        <div>
+            {listOfRestaurants.length === 0 ? (
+                <p>Please Wait</p>
+            ) : (
+                <div>
+                    <p className="font-bold text-xl">{Restaurants.card.header.title}</p>
+                    <ul>
+                        {listOfRestaurants.map((res, index) => (
+                            <li key={index} className="text-green-600">
+                                {res?.info?.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Body;
